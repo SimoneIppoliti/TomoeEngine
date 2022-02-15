19aac70637aacfc7d7bc40c73b7c8850dc8b36b2
Simone Ippoliti <simoneippoliti53@gmail.com>
2022-02-15 14:34:40 +0100

    Add and implement logging library

--- EnginePrototype/src/EnginePrototype.h
+++ EnginePrototype/src/EnginePrototype.h
@@ -3,6 +3,7 @@
 // for use by applications
 
 #include "EnginePrototype/Application.h"
+#include "EnginePrototype/Log.h"
 
 // ---Entry Point-------------------------
 #include "EnginePrototype/EntryPoint.h"
--- EnginePrototype/src/EnginePrototype/EntryPoint.h
+++ EnginePrototype/src/EnginePrototype/EntryPoint.h
@@ -6,6 +6,11 @@ extern EnginePrototype::Application* EnginePrototype::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	EnginePrototype::Log::Init();
+	ENGINE_CORE_ERROR("Initialized Log");
+	int a = 5;
+	ENGINE_FATAL("Hello. Var={0}", a);
+
 	auto app = EnginePrototype::CreateApplication();
 	app->Run();
 	delete app;
