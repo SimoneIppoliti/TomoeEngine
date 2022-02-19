bb8ad44e337fecce18052840acbb6b6a374610ba
Simone Ippoliti <simoneippoliti53@gmail.com>
2022-02-19 19:12:43 +0100

    Build solution with Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "EnginePrototype"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "EnginePrototype"
+    location "EnginePrototype"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "ENGINE_PLATFORM_WINDOWS",
+            "ENGINE_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+    filter "configurations:Debug"
+        defines "ENGINE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ENGINE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ENGINE_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "EnginePrototype/vendor/spdlog/include",
+        "EnginePrototype/src"
+    }
+
+    links
+    {
+        "EnginePrototype"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "ENGINE_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "ENGINE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ENGINE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ENGINE_DIST"
+        optimize "On"
