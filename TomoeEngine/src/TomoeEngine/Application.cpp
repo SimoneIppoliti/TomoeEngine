4520f098f7d16942e4b903325986913cc7c90e67
Sgabbo <simone.ippoliti@tinybullstudios.com>
2023-09-25 11:06:20 +0200

    Add test event and event logging

--- TomoeEngine/src/TomoeEngine/Application.cpp
+++ TomoeEngine/src/TomoeEngine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "TomoeEngine/Events/ApplicationEvent.h"
+#include "TomoeEngine/Log.h"
+
 namespace TomoeEngine {
 
 	Application::Application() {
@@ -11,6 +14,9 @@ namespace TomoeEngine {
 	}
 
 	void Application::Run() {
+		WindowResizeEvent e(1280, 720);
+		TOMOE_TRACE(e);
+
 		while (true);
 	}
 
