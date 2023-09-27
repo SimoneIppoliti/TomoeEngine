44a4974b4b1ae4dc728d3de3dc49623ffd1224c6
Sgabbo <simone.ippoliti@tinybullstudios.com>
2023-09-27 18:53:12 +0200

    Add GLFW event bindings

--- TomoeEngine/src/Platform/Windows/WindowsWindow.cpp
+++ TomoeEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,18 @@
 #include "tomoepch.h"
 #include "WindowsWindow.h"
 
+#include "TomoeEngine/Events/ApplicationEvent.h"
+#include "TomoeEngine/Events/MouseEvent.h"
+#include "TomoeEngine/Events/KeyEvent.h"
+
 namespace TomoeEngine {
 
 	static bool s_GLFWInitialized = false;
+
+	static void GLFWErrorCallback(int error, const char* description)
+	{
+		TOMOE_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+	}
 
 	Window* Window::Create(const WindowProps& props)
 	{
@@ -32,7 +41,7 @@ namespace TomoeEngine {
 		{
 			int success = glfwInit();
 			TOMOE_CORE_ASSERT(success, "Could not initialize GLFW!");
-
+			glfwSetErrorCallback(GLFWErrorCallback);
 			s_GLFWInitialized = true;
 		}
 
@@ -40,6 +49,88 @@ namespace TomoeEngine {
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		// GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+				data.Width = width;
+				data.Height = height;
+
+				WindowResizeEvent ev(width, height);
+				data.EventCallback(ev);
+			});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+				WindowCloseEvent ev;
+				data.EventCallback(ev);
+			});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+				case GLFW_PRESS:
+					{
+						KeyPressedEvent ev(key, 0);
+						data.EventCallback(ev);
+						break;
+					}
+				case GLFW_RELEASE:
+					{
+						KeyReleasedEvent ev(key);
+						data.EventCallback(ev);
+						break;
+					}
+				case GLFW_REPEAT:
+					{
+						KeyPressedEvent ev(key, 1);
+						data.EventCallback(ev);
+						break;
+					}
+				}
+			});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+				case GLFW_PRESS:
+					{
+						MouseButtonPressedEvent ev(button);
+						data.EventCallback(ev);
+						break;
+					}
+				case GLFW_RELEASE:
+					{
+						MouseButtonReleasedEvent ev(button);
+						data.EventCallback(ev);
+						break;
+					}
+				}
+			});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				MouseScrolledEvent ev((float)xOffset, (float)yOffset);
+				data.EventCallback(ev);
+			});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				MouseMovedEvent ev((float)xPos, (float)yPos);
+				data.EventCallback(ev);
+			});
 	}
 
 	void WindowsWindow::Shutdown()
--- TomoeEngine/src/TomoeEngine/Application.cpp
+++ TomoeEngine/src/TomoeEngine/Application.cpp
@@ -1,19 +1,28 @@
 #include "tomoepch.h"
 #include "Application.h"
 
-#include "TomoeEngine/Events/ApplicationEvent.h"
 #include "TomoeEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace TomoeEngine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() { }
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		TOMOE_CORE_TRACE("{0}", e);
+	}
 
 	void Application::Run()
 	{
@@ -23,5 +32,11 @@ namespace TomoeEngine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
