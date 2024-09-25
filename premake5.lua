7dffc7ad2eb74c6204599e513510fbe0f6335fae
Simone <simoneippoliti53@gmail.com>
2024-09-26 00:05:40 +0200

    Improve postbuild command fix

--- premake5.lua
+++ premake5.lua
@@ -73,8 +73,7 @@ project "TomoeEngine"
 
         postbuildcommands
         {
-            ("IF NOT EXIST ../bin/" .. outputdir .. "/Sandbox mkdir ../bin/" .. outputdir .. "/Sandbox"),
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
         }
     
     filter "configurations:Debug"
